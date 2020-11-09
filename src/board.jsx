4a1da6957fbef64d85c51f26763d0b6816f43d5a
Emmanuel Delmas <emmanuel.delmas@jobteaser.com>
2020-11-09 11:05:10 +0100

    clean(board): fix build warning

--- src/board.jsx
+++ src/board.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './board.module.css';
-import ReactDOM from 'react-dom';
 import Draggable from 'react-draggable';
 
 class Board extends React.Component {
@@ -18,16 +17,16 @@ class Board extends React.Component {
         <Draggable><p className={styles.title} style={{ left: 'calc(70vw*0.9)' }}>Litanie des saints</p></Draggable>
         <Draggable><p className={styles.title} style={{ left: 'calc(80vw*0.9)' }}>Parole de Dieu</p></Draggable>
         <Draggable><p className={styles.title} style={{ left: 'calc(90vw*0.9)' }}>Cierge allumé</p></Draggable>
-        <Draggable><img src="/image1.jpg" className={styles.image} style={{ left: 'calc(9vw*0.9)' }} draggable="false" /></Draggable>
-        <Draggable><img src="/image2.jpg" className={styles.image} style={{ left: 'calc(18vw*0.9)' }} draggable="false" /></Draggable>
-        <Draggable><img src="/image3.jpg" className={styles.image} style={{ left: 'calc(27vw*0.9)' }} draggable="false" /></Draggable>
-        <Draggable><img src="/image4.jpg" className={styles.image} style={{ left: 'calc(36vw*0.9)' }} draggable="false" /></Draggable>
-        <Draggable><img src="/image5.jpg" className={styles.image} style={{ left: 'calc(45vw*0.9)' }} draggable="false" /></Draggable>
-        <Draggable><img src="/image6.jpg" className={styles.image} style={{ left: 'calc(54vw*0.9)' }} draggable="false" /></Draggable>
-        <Draggable><img src="/image7.jpg" className={styles.image} style={{ left: 'calc(63vw*0.9)' }} draggable="false" /></Draggable>
-        <Draggable><img src="/image8.jpg" className={styles.image} style={{ left: 'calc(72vw*0.9)' }} draggable="false" /></Draggable>
-        <Draggable><img src="/image9.jpg" className={styles.image} style={{ left: 'calc(81vw*0.9)' }} draggable="false" /></Draggable>
-        <Draggable><img src="/image10.jpg" className={styles.image} style={{ left: 'calc(90vw*0.9)' }} draggable="false" /></Draggable>
+        <Draggable><img src="/image1.jpg" alt="" className={styles.image} style={{ left: 'calc(9vw*0.9)' }} draggable="false" /></Draggable>
+        <Draggable><img src="/image2.jpg" alt="" className={styles.image} style={{ left: 'calc(18vw*0.9)' }} draggable="false" /></Draggable>
+        <Draggable><img src="/image3.jpg" alt="" className={styles.image} style={{ left: 'calc(27vw*0.9)' }} draggable="false" /></Draggable>
+        <Draggable><img src="/image4.jpg" alt="" className={styles.image} style={{ left: 'calc(36vw*0.9)' }} draggable="false" /></Draggable>
+        <Draggable><img src="/image5.jpg" alt="" className={styles.image} style={{ left: 'calc(45vw*0.9)' }} draggable="false" /></Draggable>
+        <Draggable><img src="/image6.jpg" alt="" className={styles.image} style={{ left: 'calc(54vw*0.9)' }} draggable="false" /></Draggable>
+        <Draggable><img src="/image7.jpg" alt="" className={styles.image} style={{ left: 'calc(63vw*0.9)' }} draggable="false" /></Draggable>
+        <Draggable><img src="/image8.jpg" alt="" className={styles.image} style={{ left: 'calc(72vw*0.9)' }} draggable="false" /></Draggable>
+        <Draggable><img src="/image9.jpg" alt="" className={styles.image} style={{ left: 'calc(81vw*0.9)' }} draggable="false" /></Draggable>
+        <Draggable><img src="/image10.jpg" alt="" className={styles.image} style={{ left: 'calc(90vw*0.9)' }} draggable="false" /></Draggable>
         <Draggable><p className={styles.quote} style={{ left: 'calc(10vw*0.9)' }}>«Vous êtes une création nouvelle dans le Christ, vous avez revêtu le Christ»</p></Draggable>
         <Draggable><p className={styles.quote} style={{ left: 'calc(20vw*0.9)' }}>«Acclamons la parole de Dieu»</p></Draggable>
         <Draggable><p className={styles.quote} style={{ left: 'calc(30vw*0.9)' }}>«Vous êtes membres du corps du Christ... Dieu vous marque de l'huile du salut»</p></Draggable>
